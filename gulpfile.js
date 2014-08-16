96476db8c22e176b6e7b4274841e33ed73bf6e1f
gfestari <cilindrox@gmail.com>
2014-08-16 11:49:09 -0300

    Add client route for frontend
    Misc syntax cleanup
    
      - Update require order
      - Update gulpfile doc message
      - Syntax sugar for `/api` Router declaration

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,6 @@
-// Wrapper for the actual gulpfile. Add your tasks in `gulpfile.coffee`.
+
+// Wrapper for the actual gulpfile.
+// Add your tasks in `gulpfile.coffee`!
+
 require('coffee-script/register');
 require('./gulpfile.coffee');
--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,8 @@
 'use strict';
 
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+  , router = express.Router()
+  ;
 
 var Atm = require('../lib/atm');
 
